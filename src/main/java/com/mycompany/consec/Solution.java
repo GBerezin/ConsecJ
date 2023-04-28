3b28dc30566a0ded41a5a3d3b48d2588ec3903da
Georgiy <56176890+GBerezin@users.noreply.github.com>
2023-04-28 10:57:19 +0300

    1

--- src/main/java/com/mycompany/consec/Solution.java
+++ src/main/java/com/mycompany/consec/Solution.java
@@ -1,12 +1,11 @@
 package com.mycompany.consec;
 
 public final class Solution {
-    Jama.Matrix m_u; // Деформации
 
     // Конструктор класса
 
     Solution(Jama.Matrix D, Jama.Matrix F) {
-        m_u = sol(D, F);
+        sol(D, F);
     }
 
     Jama.Matrix sol(Jama.Matrix D, Jama.Matrix F) {
